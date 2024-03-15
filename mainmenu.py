79bb0261748d6708aab29fa9139df9b55074dbf3
chrisbuczy1 <chris.buczy@gmail.com>
2024-03-14 23:36:08 -0400

    fixed 'choice' is not defined bug

--- mainmenu.py
+++ mainmenu.py
@@ -1,8 +1,8 @@
 def menu():
     game = input('What game would you like to play? \n(b = blackjack)\n(p = poker)\n:')
     if game.lower() == 'b':
-        exec(open('blackjack.py').read())
+        import blackjack
     elif game.lower() == 'p':
-        exec(open('poker.py').read())
+        import poker
 
 menu()
