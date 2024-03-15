d2bfdd74eb1a2914cb2303067eb1ec803e1ca84a
chrisbuczy1 <chris.buczy@gmail.com>
2024-03-15 15:39:47 -0400

    able to add dice to hand in yatzhee

--- yahtzee.py
+++ yahtzee.py
@@ -4,13 +4,17 @@ import pandas as pd
 class yahtzee():
     dice = [1, 2, 3, 4, 5, 6]
     # roll = choice(dice)
-    table_dice = []
-    player_dice = []
     player2_dice = []
     n = 5
 
     def play(self):
-        print(choices(population=self.dice, k=5))
+        table_dice = choices(population=self.dice, k=5)
+        print(table_dice)
+        player_dice = input('Which dice would you like to keep\n(type number with spaces between)? ')
+        player_dice = player_dice.split()
+        player_dice = [int(x) for x in player_dice]
+
+
         
 
 
