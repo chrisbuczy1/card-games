cd0fb414d012b6d6e8f1168131e80408f7aadf0e
chrisbuczy1 <chris.buczy@gmail.com>
2024-03-15 11:32:11 -0400

    deleted poker, deciding what card game to choose

--- mainmenu.py
+++ mainmenu.py
@@ -2,7 +2,5 @@ def menu():
     game = input('What game would you like to play? \n(b = blackjack)\n(p = poker)\n:')
     if game.lower() == 'b':
         import blackjack
-    elif game.lower() == 'p':
-        import poker
 
 menu()
