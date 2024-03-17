bbe231d59464060ad291bce52f3a758be74577fe
chrisbuczy1 <chris.buczy@gmail.com>
2024-03-16 21:36:19 -0400

    trying to fix up adding dice to hand

--- yahtzee.py
+++ yahtzee.py
@@ -7,12 +7,24 @@ class yahtzee():
     player2_dice = []
     n = 5
 
+    def roll(self):
+        self.table_dice = choices(population=self.dice, k=5)
+        
+
     def play(self):
-        table_dice = choices(population=self.dice, k=5)
-        print(table_dice)
+        self.roll()
+        print(self.table_dice)
         player_dice = input('Which dice would you like to keep\n(type number with spaces between)? ')
         player_dice = player_dice.split()
         player_dice = [int(x) for x in player_dice]
+        for x in player_dice:    
+            while x not in self.table_dice:
+                player_dice = input('Try again: ')
+                player_dice = player_dice.split()
+                player_dice = [int(x) for x in player_dice]
+                print(player_dice)
+
+        print(player_dice)
 
 
         
