0ecb3e654b39a3e5ac934a299b25de9a014a92c6
chrisbuczy1 <chris.buczy@gmail.com>
2024-03-28 21:02:39 -0400

    dice kept from before

--- yahtzee.py
+++ yahtzee.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 class yahtzee():
     dice = [1, 2, 3, 4, 5, 6]
-    n = 5
     rolls = 3
     x = 5
+    dices = []
 
     def roll(self):
         self.table_dice = choices(population=self.dice, k=self.x)
@@ -15,24 +15,26 @@ class yahtzee():
         self.player_dice = input('Which dice would you like to keep\n(type number with spaces between)? ')
         self.player_dice = self.player_dice.split()
         self.player_dice = [int(x) for x in self.player_dice]
+        self.dices.extend(self.player_dice)
 
         
 
     def play(self):
         while self.rolls != 0:
             self.roll()
-            print(self.table_dice)
+            print(f'\n{self.table_dice}')
             self.keep()
             for num in self.player_dice:
                 if num in self.table_dice:
                     self.table_dice.remove(num)
                     self.x = len(self.table_dice)
-                    continue
                 else:
                     self.keep()
+                    break
             self.rolls -= 1
-            print(self.rolls)
+            print(f'Rolls left: {self.rolls}')
             print(self.player_dice)
+        
 
 
         
